fac521f68779f7436b8f6bc0e558998833efa6d4
Avishake2017 <avishakedutta@gmail.com>
2019-08-29 12:07:26 +0530

    initial commit

---
+++ src/app/new-component/new-component.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component
+({
+   selector: 'new-component',
+   templateUrl : './new-component.component.html',
+   styleUrls : ['./new-component.component.css']
+})
+
+export class NewComponent
+{
+title = 'Avishake';
+}
